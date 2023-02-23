f9b41696c0111fa68e0bebfd1417eac1d6b5adeb
Frank Siyli <frank.siyli@gmail.com>
2023-02-24 00:19:05 +0100

    flipping hell

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const bookmarkEmpty = document.querySelector('[data-js="bookmarkButtonEmpty"]');
-
+const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
+const bookmarkSVG = document.querySelector('[data-js="bookmarkSVG"]');
 
 
 function bookmarkButtonClick(event) {
-    const bookmarkEmpty = event.target;
-    bookmarkEmpty.classList.toggle("bookmarkBtn--active");
-  }
+    const bookmark = event.target;
+    bookmark.classList.toggle("bookmark--active");
+    // card.classList.removeClass('is-flipped');
+  };
+
+
+  bookmarkButton.addEventListener("click", bookmarkButtonClick);
+
+  const card = document.querySelector(".cardInner");
+
+  card.addEventListener("click", ()=> {
+    card.classList.toggle('is-flipped');
+  });
+
   
-  bookmarkEmpty.addEventListener("click", bookmarkButtonClick);
+
