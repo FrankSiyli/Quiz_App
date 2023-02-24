86b2301a907658b8232d0ca8fb13d5f2cb943f04
Frank Siyli <frank.siyli@gmail.com>
2023-02-25 00:31:38 +0100

    fix card

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
 const bookmarkSVG = document.querySelector('[data-js="bookmarkSVG"]');
-
+const newQButton = document.querySelector('[data-js="newQ"]');
 
 function bookmarkButtonClick(event) {
-    const bookmark = event.target;
-    bookmark.classList.toggle("bookmark--active");
-    // card.classList.removeClass('is-flipped');
-  };
+  const bookmark = event.target;
+  bookmark.classList.toggle("bookmark--active");
+  bookmark.style.transition = "1s, ease, 1s";
+  bookmark.style.animation = "600ms pulsate";
+  onclick = event.stopPropagation();
+}
 
+bookmarkButton.addEventListener("click", bookmarkButtonClick);
 
-  bookmarkButton.addEventListener("click", bookmarkButtonClick);
+const card = document.querySelector(".cardInner");
 
-  const card = document.querySelector(".cardInner");
-
-  card.addEventListener("click", ()=> {
-    card.classList.toggle('is-flipped');
-  });
-
-  
-
+card.addEventListener("click", () => {
+  card.classList.toggle("is-flipped");
+});
